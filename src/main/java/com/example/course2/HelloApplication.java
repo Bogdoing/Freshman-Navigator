c856f2bf17d836351a53a0d6dd97115b1b5185d0
Bogdoing <bogdoingg@gmail.com>
2023-06-11 21:33:25 +0300

    replay repo

--- src/main/java/com/example/course2/HelloApplication.java
+++ src/main/java/com/example/course2/HelloApplication.java
@@ -12,7 +12,7 @@ public class HelloApplication extends Application {
     public void start(Stage stage) throws IOException {
         FXMLLoader fxmlLoader = new FXMLLoader(HelloApplication.class.getResource("hello-view.fxml"));
         Scene scene = new Scene(fxmlLoader.load(), 600, 760);
-        stage.setTitle("Навигатор первокурсника");
+        stage.setTitle("Навигатор первокурсника НП");
         stage.setScene(scene);
         stage.show();
     }
